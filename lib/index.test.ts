f7a690e39da3cb8fcbcb316eb7b4d235491f12ed
Paul Adams <ghusiyy@gmail.com>
2023-06-04 00:10:04 -0700

    add isDigit, isNumeric, and isFormulaValid helper functions and unit tests

--- lib/index.test.ts
+++ lib/index.test.ts
@@ -1,8 +1,57 @@
 import { assertEquals } from "https://deno.land/std@0.113.0/testing/asserts.ts";
-import { isValidLength } from "./index.ts";
+import { isDigit, isFormulaValid, isNumeric, isValidLength } from "./index.ts";
 
 Deno.test("Card number length", () => {
   assertEquals(true, isValidLength("1234567891011"));
   assertEquals(false, isValidLength("123"));
   assertEquals(false, isValidLength("12345678910111213"));
 });
+
+Deno.test("Is digit", () => {
+  assertEquals(true, isDigit(1));
+  assertEquals(true, isDigit(0));
+  assertEquals(false, isDigit(-1));
+  assertEquals(false, isDigit(10));
+  assertEquals(true, isDigit(9));
+});
+
+Deno.test("Is numeric", () => {
+  assertEquals(true, isNumeric("1234567"));
+  assertEquals(false, isNumeric("123a567"));
+});
+
+Deno.test("isFormulaValid should return true for a valid card number", () => {
+  const validNumber = "4532015112830366";
+  const result = isFormulaValid(validNumber);
+  assertEquals(result, true);
+});
+
+Deno.test(
+  "isFormulaValid should return false for an invalid card number",
+  () => {
+    const invalidNumber = "4532015112830365";
+    const result = isFormulaValid(invalidNumber);
+    assertEquals(result, false);
+  }
+);
+
+Deno.test("isFormulaValid should return false for an empty string", () => {
+  const emptyString = "";
+  const result = isFormulaValid(emptyString);
+  assertEquals(result, false);
+});
+
+Deno.test("isFormulaValid should return false for a non-numeric input", () => {
+  const nonNumericInput = "4532ABCD5112830366";
+  const result = isFormulaValid(nonNumericInput);
+  assertEquals(result, false);
+});
+
+Deno.test(
+  "isFormulaValid should return false for a card number with only one digit",
+  () => {
+    const singleDigitNumber = "4";
+    const result = isFormulaValid(singleDigitNumber);
+    assertEquals(result, false);
+  }
+);
--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,4 @@
-export function isValidLength(cardNumber: string) {
+export function isValidLength(cardNumber: string): boolean {
   const len = cardNumber.length;
   if (len < 13 || len > 16) {
     return false;
@@ -6,9 +6,54 @@ export function isValidLength(cardNumber: string) {
   return true;
 }
 
-export function CardCheckr(cardNumber: number) {
+export function isDigit(digit: number): boolean {
+  if (digit >= 0 && digit <= 9) {
+    return true;
+  }
+  return false;
+}
+
+export function isNumeric(cardNumber: string): boolean {
+  for (const n of cardNumber) {
+    if (!isDigit(Number(n))) {
+      return false;
+    }
+  }
+  return true;
+}
+
+export function isFormulaValid(cardNumber: string): boolean {
+  if (!cardNumber) {
+    return false;
+  }
+  const digits = cardNumber.split("").map(Number);
+
+  let isDoubleDigit = false;
+  let sum = 0;
+
+  for (let i = digits.length - 1; i >= 0; i--) {
+    let digit = digits[i];
+
+    if (isDoubleDigit) {
+      digit *= 2;
+      if (digit > 9) {
+        digit -= 9;
+      }
+    }
+
+    sum += digit;
+    isDoubleDigit = !isDoubleDigit;
+  }
+
+  return sum % 10 === 0;
+}
+
+export function CardCheckr(cardNumber: number): boolean {
   const cardNumberStr = String(cardNumber);
   if (!isValidLength(cardNumberStr)) {
+    return false;
+  }
+  if (!isNumeric(cardNumberStr)) {
     return false;
   }
   return true;
